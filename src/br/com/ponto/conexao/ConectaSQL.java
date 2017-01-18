3a9ea35a3d0fd889c24d88ffb21b6ccaf4f94a9f
Marshall Paiva <marshallpaiva@hotmail.com>
2017-01-18 11:16:50 -0300

    Criando tela de batida do funcionário

--- src/br/com/ponto/conexao/ConectaSQL.java
+++ src/br/com/ponto/conexao/ConectaSQL.java
@@ -10,8 +10,7 @@ public class ConectaSQL {
         try {
             //Responsavel pela conexao
             return DriverManager.getConnection("jdbc:postgresql://localhost/nutec", "root", "debian23");
-            //return DriverManager.getConnection("jdbc:postgresql://192.168.1.11:5432/nutec", "sigi", "NtGeCsIgI");
-
+            
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
